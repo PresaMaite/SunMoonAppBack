9acc0b2aaba6617be559c6c66217fba6f1b58e59
PresaMaite <m.presa@hotmail.com>
2023-09-19 19:46:48 +0200

    server config

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// imports
 const { connection } = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
+// DB connection
 connection();
+
+// server
+const app = express();
+const port = 3900;
+
+// cors
+app.use(cors());
+
+// js objects 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// listener 
+app.listen(port, () => {
+    console.log(`port: ${port}`);
+})
